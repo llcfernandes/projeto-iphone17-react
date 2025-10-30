6a6129269b62fafda3bc21ed9adefb50459c8750
llcfernandes <lucasmividi@gmail.com>
2025-10-29 23:04:18 -0300

    adicao de mais cards de informacoes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,7 @@
 function Intro() {
 
     const specs = [
-      {value: '6.3"', label: "Display Super Retina XDR", color: "text-blue-600" },
+      { value: '6.3"', label: "Display Super Retina XDR", color: "text-blue-600" },
       { value: "A18 Pro", label: "Chip mais rápido", color: "text-orange-500" },
       { value: "48MP", label: "Sistema de câmera", color: "text-blue-600" },
       { value: "29h", label: "Bateria", color: "text-orange-500" },
