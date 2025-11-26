7016cba5c4c06b3b796afbf9292d341c49de94dc
llcfernandes <lucasmividi@gmail.com>
2025-11-26 17:38:52 -0300

    adicao de uma nova secao para mostrar as cores do iphone 17

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero"
 import Intro from "./components/Intro";
 import Highlights from "./components/Highlights";
+import Colors from "./components/Colors";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Hero />
         <Intro />
         <Highlights />
+        <Colors />
       </div>
     </>
   );
